57fd34501fb78294c87c82c885f8d067c0b07354
Daniel Kurth <daniel.kurth@sap.com>
2024-02-16 00:36:19 +0100

    add animation and text sections

--- src/AnimationSection.jsx
+++ src/AnimationSection.jsx
@@ -8,11 +8,15 @@ gsap.registerPlugin(ScrollTrigger);
 const AnimationSection = (props) => {
     const sectionRef = useRef(null);
     const canvasRef = useRef(null);
+    const textRef = useRef(null);
     const [currentFrame, _setCurrentFrame] = useState({ frame: 0 });
     const [images, setImages] = useState([]);
     const frameCount = props.frameCount;
 
-    const currentFrameImage = (index) => `${import.meta.env.BASE_URL}${props.imagesPath}${(index+1).toString().padStart(4, '0')}.${props.imageType}`;
+    const currentFrameImage = (index) =>
+        `${import.meta.env.BASE_URL}${props.imagesPath}${(index + 1)
+            .toString()
+            .padStart(4, "0")}.${props.imageType}`;
 
     // Load all images once after the first render and draw thumbnail
     useEffect(() => {
@@ -40,40 +44,71 @@ const AnimationSection = (props) => {
     useGSAP(() => {
         const canvas = canvasRef.current;
         const context = canvas.getContext("2d");
-        console.log(currentFrame.frame)
 
         const render = () => {
             if (images[currentFrame.frame]) {
                 context.clearRect(0, 0, canvas.width, canvas.height);
-                context.drawImage(images[currentFrame.frame], 0, 0);
+                context.drawImage(
+                    images[currentFrame.frame],
+                    0,
+                    0,
+                    canvas.width,
+                    canvas.height
+                );
             }
         };
 
-        gsap.timeline({
+        const tl = gsap.timeline({
             onUpdate: render,
             scrollTrigger: {
                 trigger: sectionRef.current,
                 pin: true,
-                scrub: 0.5,
-                end: "+=1000",
-                markers: true,
+                scrub: 1,
+                end: props.scrollingLength,
+                markers: false,
             },
-        })
-            .to(
-                currentFrame,
-                {
-                    frame: frameCount - 1,
-                    snap: "frame",
-                    ease: "none",
-                    duration: 1,
-                },
-                0
-            );
+        });
+
+        // Add fade out effect to the text
+        tl.fromTo(
+            textRef.current,
+            { opacity: 1 },
+            { opacity: 0, duration: 0.3 },
+            0
+        );
+
+        // Add animation to change the frame
+        tl.to(
+            currentFrame,
+            {
+                frame: frameCount - 1,
+                snap: "frame",
+                ease: "none",
+                duration: 1,
+            },
+            0
+        );
     }, [frameCount, images]);
 
     return (
-        <div ref={sectionRef}>
-            <canvas ref={canvasRef}></canvas>
+        <div ref={sectionRef} style={{ position: 'relative', width: '100%', height: '100%' }}>
+            <canvas ref={canvasRef} style={{ width: "100%", height: "auto" }}></canvas>
+            <div
+                ref={textRef}
+                style={{
+                    fontFamily: "Arial",
+                    position: "absolute",
+                    top: "1em",
+                    left: "2em",
+                    fontSize: "4em",
+                    color: "white",
+                    whiteSpace: "nowrap",
+                    backgroundColor: "transparent",
+                    textShadow: "2px 2px 2px #CE5937",
+                }}
+            >
+                {props.children}
+            </div>
         </div>
     );
 };
--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,20 @@ import AnimationSection from "./AnimationSection.jsx";
 
 const App = () => (
     <main>
-        <AnimationSection frameCount={100} imagesPath={"images2/"} imageType={"jpg"}/>
+        <AnimationSection frameCount={100} imagesPath={"images/car_1/"} imageType={"png"} scrollingLength={"+=5000"}>
+            Airtsel Valor Proiettile
+        </AnimationSection>
+
         <div className="text">
-            <h1> TEST</h1>
+            <h2>Introducing the Airtsel Valor Proiettile, a quintessential icon of 1960s automotive excellence. With its sleek design and unparalleled performance, this sport car exemplifies the epitome of speed and style.</h2>
         </div>
-        <AnimationSection frameCount={100} imagesPath={"images2/"} imageType={"jpg"}/>
+
+        <AnimationSection frameCount={51} imagesPath={"images/car_2/"} imageType={"png"} scrollingLength={"+=5000"}>
+            Interior Design
+        </AnimationSection>
+
         <div className="text">
-            <h1> TEST</h1>
+            <h2>Step into the Airtsel Valor Proiettile and experience its Old Leather brownisch classic interior—a blend of timeless charm and refined luxury. Sink into plush leather seats, adorned with meticulous stitching and rich, warm tones. Polished wood accents and chrome embellishments on the dashboard evoke a sense of nostalgia, while the vintage-inspired steering wheel offers both tactile feedback and precision. Whether cruising the streets or hitting the track, this interior ensures every journey is a memorable one.</h2>
         </div>
     </main>
 );
