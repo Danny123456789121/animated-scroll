2f7ed32b6c583c330e6258f6c52e28d9b46324e9
Daniel Kurth <daniel.kurth@sap.com>
2024-02-16 00:39:44 +0100

    add animation section 3

--- src/App.jsx
+++ src/App.jsx
@@ -19,6 +19,14 @@ const App = () => (
         <div className="text">
             <h2>Step into the Airtsel Valor Proiettile and experience its Old Leather brownisch classic interior—a blend of timeless charm and refined luxury. Sink into plush leather seats, adorned with meticulous stitching and rich, warm tones. Polished wood accents and chrome embellishments on the dashboard evoke a sense of nostalgia, while the vintage-inspired steering wheel offers both tactile feedback and precision. Whether cruising the streets or hitting the track, this interior ensures every journey is a memorable one.</h2>
         </div>
+
+        <AnimationSection frameCount={51} imagesPath={"images/car_3/"} imageType={"png"} scrollingLength={"+=5000"}>
+            Interior Design
+        </AnimationSection>
+
+        <div className="text">
+            <h2>Step into the Airtsel Valor Proiettile and experience its Old Leather brownisch classic interior—a blend of timeless charm and refined luxury. Sink into plush leather seats, adorned with meticulous stitching and rich, warm tones. Polished wood accents and chrome embellishments on the dashboard evoke a sense of nostalgia, while the vintage-inspired steering wheel offers both tactile feedback and precision. Whether cruising the streets or hitting the track, this interior ensures every journey is a memorable one.</h2>
+        </div>
     </main>
 );
 export default App;
